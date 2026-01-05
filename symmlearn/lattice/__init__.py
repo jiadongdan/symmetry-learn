d4519742e3a48b4280cbac465174f63759793db5
Jiadong Dan <ted930511@gmail.com>
2026-01-05 11:21:46 +0800

    update find translation vector

--- symmlearn/lattice/__init__.py
+++ symmlearn/lattice/__init__.py
@@ -5,6 +5,7 @@ from ._mixin_plane_group import generate_plane_group_cell
 from ._plane_group import is_new_atoms_better
 from ._pg_image import PGLattice
 from ._pg_image import PGImage
+from ._pg_image import find_translation_vector
 from ._layer_group import get_layer_group, get_plane_group
 from ._utils import plane2layer, layer2plane
 from ._crystal_systems import random_structure_A
@@ -18,5 +19,6 @@ __all__ = ['PlaneGroup',
            'PGImage',
            'get_layer_group',
            'get_plane_group',
-           'random_structure_A'
+           'random_structure_A',
+           'find_translation_vector'
            ]

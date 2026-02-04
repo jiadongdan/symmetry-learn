116a1e5c669b0856382515ff4b185da6b47b2271
Jiadong Dan <ted930511@gmail.com>
2026-02-04 15:18:17 +0800

    add check_random_state() form sklearn

--- symmlearn/utils/__init__.py
+++ symmlearn/utils/__init__.py
@@ -1,8 +1,9 @@
 from ._show_atoms import show_atoms
-from ._ramdom import check_random_generator
+from ._ramdom import check_random_generator, check_random_state
 from ._estimate_file_size import estimate_file_size
 
 __all__ = ['show_atoms',
            'check_random_generator',
+           'check_random_state',
            'estimate_file_size'
            ]
